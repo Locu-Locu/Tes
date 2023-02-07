a333fa0c4a03407a0b07e683a48b98cb34b10ede
Locu <122764123+Locu-Locu@users.noreply.github.com>
2023-02-08 05:01:59 +0700

    Update install-sldns.sh

--- slowdnss/install-sldns.sh
+++ slowdnss/install-sldns.sh
@@ -1,154 +1,318 @@
 #!/bin/bash
-# Slowdns Instalation by Jrtunnel 19-09-2022
-# ==========================================
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+rm -rf /etc/apt/sources.list
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/Locu-Locu/Tes/main/sources.list"
+apt-get update -y && apt-get upgrade -y
+apt-get install -y cmake jq socat wget bzip2 gzip coreutils screen dpkg wget vim curl nano zip unzip 
 
-clear
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
 clear
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
+tyblue='\e[1;36m'
 NC='\e[0m'
-echo -e " [ ${green}INFO${NC} ] Installing SSH Slow DNS "
-echo "Progress..." | lolcat
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Downloading files... "
-wget -qc https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/hostdnss.sh && chmod +x hostdnss.sh &&  sed -i -e 's/\r$//' hostdnss.sh && ./hostdnss.sh
-nameserver=$(cat /home/nsdomain)
-echo -e "[ ${green}INFO${NC} ] Download File... "
-echo "Progress..." | lolcat
-# SSH SlowDNS
-echo -e " [ ${green}INFO${NC} ] Successfully.. "
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/sshd_config
-systemctl restart sshd
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Tambahan... "
-apt install screen -y
-apt install cron -y
-apt install iptables -y
-service cron reload
-service cron restart
-service iptables reload
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Downloading files... "
-cd /usr/local
-wget -qc https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
-tar xvf go1.16.2.linux-amd64.tar.gz
-export GOROOT=/usr/local/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 cd /root
-apt install git -y
+#System version number
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+
+localip=$(hostname -I | cut -d\  -f1)
+hst=( `hostname` )
+dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
+if [[ "$hst" != "$dart" ]]; then
+echo "$localip $(hostname)" >> /etc/hosts
+fi
+mkdir -p /etc/xray
+
+echo -e "[ ${tyblue}NOTES${NC} ] Before we go.. "
 sleep 1
-echo -e "[ ${green}INFO${NC} ] Downloading files... "
-git clone https://www.bamsoftware.com/git/dnstt.git jrtunnel
-mv /root/jrtunnel /root/slowdns
-rm -rf jrtunnel
-cd /root/slowdns/dnstt-server
-go build
-echo -e "[ ${green}INFO${NC} ] Install Key... "
-sleep 1
-./dnstt-server -gen-key -privkey-file /root/slowdns/dnstt-server/server.key -pubkey-file /root/slowdns/dnstt-server/server.pub
-echo -e "[ ${green}INFO${NC} ] Successfully... "
-sleep 1
-mkdir -m 777 /root/.dns
+echo -e "[ ${tyblue}NOTES${NC} ] I need check your headers first.."
 sleep 2
-echo -e "[ ${green}INFO${NC} ] Waiting... "
-mv /root/slowdns/dnstt-server/server.key /root/.dns/server.key
-mv /root/slowdns/dnstt-server/server.pub /root/.dns/server.pub
-rm -rf /etc/slowdns
-mkdir -m 777 /etc/slowdns
-echo -e "[ ${green}INFO${NC} ] Successfully... "
+echo -e "[ ${green}INFO${NC} ] Checking headers"
 sleep 1
-cd /root
-rm -rf slowdns
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Downloading files... "
-wget -qc -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/sldns-server"
-wget -qc -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/sldns-client"
-sleep 1
-chmod +x /etc/slowdns/sldns-server
-chmod +x /etc/slowdns/sldns-client
-cd
-echo -e "[ ${green}INFO${NC} ] Successfully... "
-sleep 1
-wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/client-sldns.service"
-wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/server-sldns.service"
-cd
-sleep 1
-echo -e "[ ${green}INFO${NC} ] System Prosess... "
+totet=`uname -r`
+REQUIRED_PKG="linux-headers-$totet"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  sleep 2
+  echo -e "[ ${yell}WARNING${NC} ] Try to install ...."
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  apt-get --yes install $REQUIRED_PKG
+  sleep 1
+  echo ""
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] If error you need.. to do this"
+  sleep 1
+  echo ""
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] 1. apt update -y"
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] 2. apt upgrade -y"
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] 3. apt dist-upgrade -y"
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] 4. reboot"
+  sleep 1
+  echo ""
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] After rebooting"
+  sleep 1
+  echo -e "[ ${tyblue}NOTES${NC} ] Then run this script again"
+  echo -e "[ ${tyblue}NOTES${NC} ] Notes, Script Mod By PapaBearS"
+  echo -e "[ ${tyblue}NOTES${NC} ] if you understand then tap enter now.."
+  read
+else
+  echo -e "[ ${green}INFO${NC} ] Oke installed"
+fi
+
+ttet=`uname -r`
+ReqPKG="linux-headers-$ttet"
+if ! dpkg -s $ReqPKG  >/dev/null 2>&1; then
+  rm /root/setup.sh >/dev/null 2>&1 
+  exit
+else
+  clear
+fi
+
+
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
+coreselect=''
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+END
+chmod 644 /root/.profile
+
+echo -e "[ ${green}INFO${NC} ] Preparing the install file"
+apt install git curl -y >/dev/null 2>&1
+sleep 3
+
+mkdir -p /etc/aixxy7
+mkdir -p /etc/aixxy7/theme
+mkdir -p /var/lib/aixxy7-pro >/dev/null 2>&1
+echo "IP=" >> /var/lib/aixxy7-pro/ipvps.conf
+
+if [ -f "/etc/xray/domain" ]; then
+echo ""
+echo -e "[ ${green}INFO${NC} ] Script Already Installed"
+echo -ne "[ ${yell}WARNING${NC} ] Do you want to install again ? (y/n)? "
+read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+rm setup.sh
+sleep 10
+exit 0
+else
+clear
+fi
+fi
+
+echo ""
+wget -q https://raw.githubusercontent.com/Locu-Locu/Tes/main/dependencies.sh;chmod +x dependencies.sh;./dependencies.sh
+rm dependencies.sh
+clear
+
+yellow "Add Domain for vmess/vless/trojan dll"
+echo "-------------------------------------"
+echo "     Sc Premium KennXV "
+echo "-------------------------------------"
+read -rp "Input your domain : " -e pp
+echo "$pp" > /root/domain
+echo "$pp" > /root/scdomain
+echo "$pp" > /etc/xray/domain
+echo "$pp" > /etc/xray/scdomain
+echo "IP=$pp" > /var/lib/aixxy7-pro/ipvps.conf
+
+#THEME RED
+cat <<EOF>> /etc/aixxy7/theme/red
+BG : \E[40;1;41m
+TEXT : \033[0;31m
+EOF
+#THEME BLUE
+cat <<EOF>> /etc/aixxy7/theme/blue
+BG : \E[40;1;44m
+TEXT : \033[0;34m
+EOF
+#THEME GREEN
+cat <<EOF>> /etc/aixxy7/theme/green
+BG : \E[40;1;42m
+TEXT : \033[0;32m
+EOF
+#THEME YELLOW
+cat <<EOF>> /etc/aixxy7/theme/yellow
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME MAGENTA
+cat <<EOF>> /etc/aixxy7/theme/magenta
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME CYAN
+cat <<EOF>> /etc/aixxy7/theme/cyan
+BG : \E[40;1;46m
+TEXT : \033[0;36m
+EOF
+#THEME CONFIG
+cat <<EOF>> /etc/aixxy7/theme/color.conf
+blue
+EOF
+    
+#install ssh ovpn
+echo -e "$green[INFO]$NC Install SSH"
 sleep 2
-install client-sldns.service
-cat > /etc/systemd/system/client-sldns.service << END
-[Unit]
-Description=Client SlowDNS By Jrtunnel Mwoi
-Documentation=t.me/
-After=network.target nss-lookup.target
+clear
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+#Instal Xray
+echo -e "$green[INFO]$NC Install XRAY!"
+sleep 2
+clear
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+clear
+#wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+clear
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/websocket/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+clear
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/websocket/nontls.sh && chmod +x nontls.sh && ./nontls.sh
+clear
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/slowdnss/install-sldns.sh && chmod +x install-sldns.sh && sed -i -e 's/\r$//' ./install-sldns.sh && ./install-sldns.sh
+clear
+echo -e "$green[INFO]$NC Download Extra Menu"
+sleep 2
+wget https://raw.githubusercontent.com/Locu-Locu/Tes/main/update/update.sh && chmod +x update.sh && ./update.sh
+clear
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+clear
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /root/.dns/server.pub $nameserver 127.0.0.1:3369
-Restart=on-failure
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
 
-[Install]
-WantedBy=multi-user.target
+mesg n || true
+clear
+menu
 END
-cd
-install server-sldns.service
-cat > /etc/systemd/system/server-sldns.service << END
-[Unit]
-Description=Server SlowDNS By Jrtunnel Mwoi
-Documentation=t.me/ajikcobain 
-After=network.target nss-lookup.target
+chmod 644 /root/.profile
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /root/.dns/server.key $nameserver 127.0.0.1:2269
-Restart=on-failure
+if [ -f "/root/log-install.txt" ]; then
+rm /root/log-install.txt > /dev/null 2>&1
+fi
+if [ -f "/etc/afak.conf" ]; then
+rm /etc/afak.conf > /dev/null 2>&1
+fi
+if [ ! -f "/etc/log-create-user.log" ]; then
+echo "Log All Account " > /etc/log-create-user.log
+fi
+history -c
+serverV=$( curl -sS https://raw.githubusercontent.com/Locu-Locu/Tes/main/version  )
+echo $serverV > /opt/.ver
+aureb=$(cat /home/re_otm)
+b=11
+if [ $aureb -gt $b ]
+then
+gg="PM"
+else
+gg="AM"
+fi
+curl -sS ifconfig.me > /etc/myipvps
 
-[Install]
-WantedBy=multi-user.target
-END
-cd
-echo -e "[ ${green}INFO${NC} ] Successfully... "
-chmod +x /etc/systemd/system/client-sldns.service
-chmod +x /etc/systemd/system/server-sldns.service
-pkill sldns-server
-pkill sldns-client
-
-iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
-
-systemctl daemon-reload
-systemctl stop client-sldns
-systemctl stop server-sldns
-systemctl enable client-sldns
-systemctl enable server-sldns
-systemctl start client-sldns
-systemctl start server-sldns
-systemctl restart client-sldns
-systemctl restart server-sldns
-cd
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Downloading files Success "
-cd
+echo " "
+echo "=====================-[ AutoScript Premium ]-===================="
+echo ""
+echo "------------------------------------------------------------"
+echo ""
+echo ""
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - SSH Websocket           : 80 [OFF]" | tee -a log-install.txt
+echo "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
+echo "   - SSH NON-SSL Websocket   : 8880" | tee -a log-install.txt
+echo "   - SLOWDNS                 : 5300" | tee -a log-install.txt
+echo "   - Stunnel4                : 447, 777" | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
+echo "   - Badvpn                  : 7100-7900" | tee -a log-install.txt
+echo "   - Nginx                   : 81" | tee -a log-install.txt
+echo "   - XRAY  Vmess TLS         : 443" | tee -a log-install.txt
+echo "   - XRAY  Vmess None TLS    : 80" | tee -a log-install.txt
+echo "   - XRAY  Vless TLS         : 443" | tee -a log-install.txt
+echo "   - XRAY  Vless None TLS    : 80" | tee -a log-install.txt
+echo "   - Trojan GRPC             : 443" | tee -a log-install.txt
+echo "   - Trojan WS               : 443" | tee -a log-install.txt
+echo "   - Sodosok WS/GRPC         : 443" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On           : $aureb:00 $gg GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - AutoKill Multi Login User" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Fully automatic script" | tee -a log-install.txt
+echo "   - VPS settings" | tee -a log-install.txt
+echo "   - Admin Control" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Contact : t.me/aixxy7 (Text Only)"  | tee -a log-install.txt
+echo ""
+echo ""
+echo "------------------------------------------------------------"
+echo ""
+echo "===============-[ AUTO SCRIPT BY KennXV  ]-==============="
+echo -e ""
+echo ""
+echo "" | tee -a log-install.txt
+rm /root/cf.sh >/dev/null 2>&1
+rm /root/setup.sh >/dev/null 2>&1
+rm /root/insshws.sh 
+rm /root/update.sh
+rm /root/nontls.sh
+rm /root/install-sldns.sh
+secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
+echo -e "
+"
+echo -ne "[ ${yell}WARNING${NC} ] Do you want to reboot now ? (y/n)? "
+read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+exit 0
+else
+reboot
+fi
