88db90731b55e033c0084609a6acfeaeb065e8b5
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-02-08 06:21:59 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -240,11 +240,11 @@ case $opt in
 07 | 7) clear ; menu-theme ;;
 08 | 8) clear ; menu-backup ;;
 09 | 9) clear ; menu-set ;;
-10 | clear ; info ;;
-11)  clear ; add-host ;;
-12)  clear ; crtxray ;;
-13) clear ; $ressee ;;
-14) clear ; $bottt ;;
+10 | 10) clear ; info ;;
+11)  11) clear ; add-host ;;
+12)  12) clear ; crtxray ;;
+13)  13) clear ; $ressee ;;
+14)  14) clear ; $bottt ;;
 100) clear ; $up2u ;; 
 00 | 0) clear ; menu ;;
 *) clear ; menu ;;
